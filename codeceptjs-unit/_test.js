ac87a676bc6671ef66cac652c4a7e004b0041ed0
Andrew Cao <ancao@Andrews-MacBook-Pro.local>
2022-02-10 22:11:58 +1100

    Finished tests

--- codeceptjs-unit/_test.js
+++ codeceptjs-unit/_test.js
@@ -1,6 +1,25 @@
+const assert = require('assert');
+
 Feature('Example Feature');
 
 Scenario('first test', ({ I }) => {
     I.amOnPage("http://example.com");
+    I.wait(1);
+    I.saveScreenshot("test.png", true);
+});
+
+Scenario('second test', ({ I }) => {
+    I.amOnPage("http://example.com");
+    I.see("Example");
+    I.dontSee("Google");
+    I.seeElement("h1");
+    I.dontSee("ideopfiwjoc");
+    I.wait(2);
+});
+
+Scenario('third test', async ({ I }) => {
+    I.amOnPage("http://example.com");
+    const text = await I.grabTextFrom('h1');
     I.wait(3);
+    assert(text, 'Example Domain', 'Text does not match')
 });
