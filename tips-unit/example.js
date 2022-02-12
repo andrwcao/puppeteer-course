8aeb9c99686112a8b8ae46ea0180422846c9548d
An Cao <acao0002@student.monash.edu>
2022-02-12 14:46:57 +1100

    Added puppeteer pdf functionality

--- tips-unit/example.js
+++ tips-unit/example.js
@@ -1,8 +1,17 @@
 const puppeteer = require("puppeteer");
-(async () => {
+
+const screenshot = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.example.com', { waitUntil: "networkidle0"});
     await page.screenshot({ path: "example.png", fullPage: true });
+    await browser.close();
+};
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.example.com', { waitUntil: "networkidle0"});
+    await page.pdf({ path: "example.pdf", format: 'A4' });
     await browser.close();
 })();
