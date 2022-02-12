1430dc26d7aaf58eff0e14fa4d595e9817664fad
An Cao <acao0002@student.monash.edu>
2022-02-12 15:48:06 +1100

    emulate and geolocation

--- tips-unit/example.js
+++ tips-unit/example.js
@@ -8,10 +8,57 @@ const screenshot = async () => {
     await browser.close();
 };
 
-(async () => {
+const pdf = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.example.com', { waitUntil: "networkidle0"});
     await page.pdf({ path: "example.pdf", format: 'A4' });
+    await browser.close();
+};
+
+const emulate = async () => {
+    const browser = await puppeteer.launch({ headless: false});
+    const page = await browser.newPage();
+    await page.emulate(puppeteer.devices["iPhone X"]);
+    await page.goto('https://www.example.com');
+    await browser.close();
+};
+
+const geolocation = async () => {
+
+    const browser = await puppeteer.launch({ headless: false});
+    const page = await browser.newPage();
+
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions('https://pptr.dev', ['geolocation']);
+
+    await page.goto('https://pptr.dev');
+    await page.waitForSelector('title');
+    await page.waitForTimeout(5000);
+    await page.setGeolocation({ latitude: 90, longitude: 0 });
+    await browser.close();
+};
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false});
+    const page = await browser.newPage();
+    await page.emulate(puppeteer.devices["iPhone X"]);
+    await page.goto('https://www.example.com');
+    await browser.close();
+})();
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false});
+    const page = await browser.newPage();
+    await page.emulate(puppeteer.devices["iPhone X"]);
+    await page.goto('https://www.example.com');
+    await browser.close();
+})();
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false});
+    const page = await browser.newPage();
+    await page.emulate(puppeteer.devices["iPhone X"]);
+    await page.goto('https://www.example.com');
     await browser.close();
 })();
